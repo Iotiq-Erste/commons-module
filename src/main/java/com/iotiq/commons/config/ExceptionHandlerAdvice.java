5109ac215ef2793fdf28923f1dbd05ceaccda0d4
inci <inci@iotiq.net>
2023-07-20 11:14:20 +0300

    use problem detail in exception handler

--- src/main/java/com/iotiq/commons/config/ExceptionHandlerAdvice.java
+++ src/main/java/com/iotiq/commons/config/ExceptionHandlerAdvice.java
@@ -1,93 +1,77 @@
 package com.iotiq.commons.config;
 
 import com.iotiq.commons.exceptions.ApplicationException;
-import com.iotiq.commons.message.response.ErrorMessageDto;
 import com.iotiq.commons.util.LoggingUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.context.NoSuchMessageException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.lang.NonNull;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.filter.ServerHttpObservationFilter;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
+import static com.iotiq.commons.util.HttpUtil.getSeries;
 import static org.springframework.context.i18n.LocaleContextHolder.getLocale;
 
 @ControllerAdvice
 @RequiredArgsConstructor
-public class ExceptionHandlerAdvice {
+public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
 
-    private static final String EXCEPTION_GENERIC = "exception.generic";
     private final ResourceBundleMessageSource messageSource;
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessageDto> handleValidationException(MethodArgumentNotValidException exception, @NonNull WebRequest request) {
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<Object> handleApplicationException(ApplicationException exception, @NonNull WebRequest request) {
         logException(request, exception);
-        Set<String> messages = exception.getBindingResult().getFieldErrors()//Validations are duplicated somehow.
-                .stream()
-                .filter(it -> org.springframework.util.StringUtils.hasText(it.getDefaultMessage()))
-                .map(fieldError -> getDescription(fieldError.getDefaultMessage())).collect(Collectors.toSet());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                ErrorMessageDto.builder()
-                        .requestId(LoggingUtils.getExistingRequestId())
-                        .messages(new ArrayList<>(messages)).build());
+
+        String defaultDetail = messageSource.getMessage(exception, getLocale());
+        String messageCode = ErrorResponse.getDefaultDetailMessageCode(ApplicationException.class, (String) null);
+        HttpStatusCode status = exception.getStatus();
+        Object[] arguments = exception.getArguments();
+
+        ProblemDetail problemDetail = this.createProblemDetail(exception, status, defaultDetail, messageCode, arguments, request);
+        return this.handleExceptionInternal(exception, problemDetail, new HttpHeaders(), status, request);
     }
 
-    @ExceptionHandler(value = DataIntegrityViolationException.class)
-    public ResponseEntity<ErrorMessageDto> handleDataIntegrityViolationException(DataIntegrityViolationException exception, @NonNull WebRequest request) {
-        Throwable cause = exception.getCause();
+    @ExceptionHandler(Throwable.class)
+    ProblemDetail onThrowable(Throwable error, HttpServletRequest request) {
+        logger.error(ExceptionUtils.getStackTrace(error));
+        ServerHttpObservationFilter.findObservationContext(request)
+                .ifPresent(context -> context.setError(error));
 
-        if (cause instanceof ConstraintViolationException constraintViolationException) {
-            SQLException sqlException = constraintViolationException.getSQLException();
-            String sqlMessage = sqlException.getMessage();
-            Pattern pattern = Pattern.compile("^(?:.+|\n+)(Detail:)(?<messageDetail>.+)$");
-            Matcher matcher = pattern.matcher(sqlMessage);
-
-            if (matcher.find()) {
-                String messageDetail = matcher.group("messageDetail");
-
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .body(ErrorMessageDto.builder()
-                                .messages(Collections.singletonList(getDescription("exception.hibernateConstraintViolation", messageDetail)))
-                                .requestId(LoggingUtils.getExistingRequestId())
-                                .build());
-            }
-        }
-
-        return handle(exception, request);
+        return createProblemDetail(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = ApplicationException.class)
-    public ResponseEntity<ErrorMessageDto> handleApplicationException(ApplicationException exception, @NonNull WebRequest request) {
-        return logAndPrepareResponse(exception, request, exception.getStatus(), messageSource.getMessage(exception, getLocale()));
+    @Override
+    protected @NotNull ProblemDetail createProblemDetail(
+            @NotNull Exception exception, @NotNull HttpStatusCode status, @NotNull String defaultDetail,
+            String detailMessageCode, Object[] detailMessageArguments, @NotNull WebRequest request
+    ) {
+        HttpStatus.Series series = getSeries(status);
+        ProblemDetail problemDetail = super.createProblemDetail(exception, status, defaultDetail, detailMessageCode, detailMessageArguments, request);
+        addProblemDetailProperties(problemDetail, series, exception);
+        return problemDetail;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorMessageDto> handle(Exception exception, @NonNull WebRequest request) {
-        return logAndPrepareResponse(exception, request, HttpStatus.INTERNAL_SERVER_ERROR, (EXCEPTION_GENERIC));
+    private ProblemDetail createProblemDetail(Throwable error, HttpStatus status) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle(status.getReasonPhrase());
+        problemDetail.setDetail(error.toString());
+        addProblemDetailProperties(problemDetail, status.series(), error);
+//        problemDetail.setProperty("traceparent", getTraceParent());
+
+        return problemDetail;
     }
 
-    private ResponseEntity<ErrorMessageDto> logAndPrepareResponse(Exception exception, WebRequest request, HttpStatus status, String description) {
-        logException(request, exception);
-        return ResponseEntity.status(status)
-                .body(ErrorMessageDto.builder()
-                        .messages(Collections.singletonList(getDescription(description)))
-                        .requestId(LoggingUtils.getExistingRequestId())
-                        .build());
+    private static void addProblemDetailProperties(ProblemDetail problemDetail, HttpStatus.Series status, Throwable error) {
+        problemDetail.setProperty("series", status);
+        problemDetail.setProperty("rootCause", ExceptionUtils.getRootCause(error).toString());
     }
 
     void logException(WebRequest request, Exception ex) {
@@ -101,11 +85,4 @@ public class ExceptionHandlerAdvice {
         LoggingUtils.error(message, ex);
     }
 
-    String getDescription(String key, String... args) {
-        try {
-            return messageSource.getMessage(key, args, getLocale());
-        } catch (NoSuchMessageException ex) {
-            return key;//If it is not defined
-        }
-    }
 }
