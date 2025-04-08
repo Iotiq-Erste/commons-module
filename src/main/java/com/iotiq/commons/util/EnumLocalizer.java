b1cd83d93df9fa68c755102d468405210cd3b4f1
yusuf <yusuf@iotiq.net>
2025-04-08 09:34:52 +0300

    feat: add support for localized enum option values in EnumLocalizer.

--- src/main/java/com/iotiq/commons/util/EnumLocalizer.java
+++ src/main/java/com/iotiq/commons/util/EnumLocalizer.java
@@ -10,12 +10,18 @@ import java.util.Locale;
 @Component
 @RequiredArgsConstructor
 public class EnumLocalizer {
-    
+
     private final MessageSource messageSource;
 
     public String getLocalizedValue(Enum<?> enumValue, String enumKey) {
         Locale locale = LocaleContextHolder.getLocale();
         String messageCode = "enum." + enumKey + "." + enumValue.name();
+        return messageSource.getMessage(messageCode, null, locale);
+    }
+
+    public String getLocalizedOptionValue(String enumValue, String optionValue, String enumKey) {
+        Locale locale = LocaleContextHolder.getLocale();
+        String messageCode = "enum." + enumKey + "." + enumValue + "." + optionValue;
         return messageSource.getMessage(messageCode, null, locale);
     }
 }
